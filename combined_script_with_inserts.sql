029e18cdc9b414b0a368935a6cc73eb19410b589
Shim2003 <shimkh-wm23@student.tarc.edu.my>
2026-04-12 13:07:06 +0800

    restaurant rating updated

--- combined_script_with_inserts.sql
+++ combined_script_with_inserts.sql
@@ -568,18 +568,18 @@ INSERT INTO Voucher VALUES ('V011', 'FREESHIP', 'Free Delivery', 0, DATE '2027-1
 
 SET DEFINE OFF;
 
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R001', 'KFJ Fast Food', 'Fast Food', 1, 'Bukit Bintang', 'Open', 4.25);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R002', 'Village Park Restaurant', 'Malay', 1, 'Damansara Utama', 'Open', 4.60);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R003', 'Din Tai Fung', 'Chinese', 0, 'Pavilion KL', 'Open', 4.80);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R004', 'Sushi Jiro', 'Japanese', 0, 'Mid Valley Megamall', 'Open', 4.10);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R005', 'Murni Discovery', 'Fusion', 1, 'SS2 Petaling Jaya', 'Closed', 4.40);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R006', 'Uncle Don''s', 'Western', 0, 'Taipan USJ', 'Open', 4.35);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R007', 'Banana Bro', 'Indian', 1, 'Berjaya Times Square', 'Open', 4.00);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R008', 'KyoChon 1991', 'Korean', 1, '1 Utama', 'Suspended', 4.70);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R009', 'The Chicken Rice Shop', 'Malaysian', 1, 'Sunway Pyramid', 'Open', 3.85);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R010', 'Nasi Lemak Antarabangsa', 'Malay', 1, 'Kampung Baru', 'Open', 4.55);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R011', 'Burger Lab', 'Fast Food', 1, 'Seapark', 'Open', 4.45);
-INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R012', 'Haidilao Hot Pot', 'Chinese', 0, 'Sunway Velocity', 'Closed', 4.90);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R001', 'KFJ Fast Food', 'Fast Food', 1, 'Bukit Bintang', 'Open', 3.10);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R002', 'Village Park Restaurant', 'Malay', 1, 'Damansara Utama', 'Open', 3.09);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R003', 'Din Tai Fung', 'Chinese', 0, 'Pavilion KL', 'Open', 3.14);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R004', 'Sushi Jiro', 'Japanese', 0, 'Mid Valley Megamall', 'Open', 2.88);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R005', 'Murni Discovery', 'Fusion', 1, 'SS2 Petaling Jaya', 'Closed', 3.28);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R006', 'Uncle Don''s', 'Western', 0, 'Taipan USJ', 'Open', 3.06);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R007', 'Banana Bro', 'Indian', 1, 'Berjaya Times Square', 'Open', 3.13);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R008', 'KyoChon 1991', 'Korean', 1, '1 Utama', 'Suspended', 2.93);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R009', 'The Chicken Rice Shop', 'Malaysian', 1, 'Sunway Pyramid', 'Open', 3.00);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R010', 'Nasi Lemak Antarabangsa', 'Malay', 1, 'Kampung Baru', 'Open', 2.94);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R011', 'Burger Lab', 'Fast Food', 1, 'Seapark', 'Open', 3.00);
+INSERT INTO Restaurant (restaurantID, restaurant_name, restaurant_category, is_halal, location_area, current_status, average_rating) VALUES ('R012', 'Haidilao Hot Pot', 'Chinese', 0, 'Sunway Velocity', 'Closed', 3.15);
 
 commit;
 
